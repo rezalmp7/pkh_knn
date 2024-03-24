5b6db3f4fc84174dd8380fc874b19b8bfb53a224
rezalmp7 <rezalwahyu10@gmail.com>
2024-03-24 11:53:22 +0700

    Fix : route

--- application/config/database.php
+++ application/config/database.php
@@ -75,7 +75,7 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => 'localhost',
+	'hostname' => 'localhost:8889',
 	'username' => 'root',
 	'password' => 'root',
 	'database' => 'pkh_knn',
